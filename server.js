92161fc8cd1d9ccc0d1e7a7171ee4c3861064703
Gabriel Jaeger <gabrieljaeger1@gmail.com>
2021-09-14 23:21:48 +0200

    trying to get production server running

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('front/build'));
 
   const path = require('path');
-  app.get('/*', (req,res) => {
+  app.get('*', (req,res) => {
     res.sendFile(path.resolve(__dirname, 'front', 'build', 'index.html'));
   });
 }
