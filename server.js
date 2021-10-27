c866263259d1c340a83909850a23933dfb3e29f9
gabrieljaegerde <gabrieljaeger1@gmail.com>
2021-10-27 15:26:07 +0200

    still fixing

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import treasureRoutes from "./routes/treasureRoutes.js";
 import { getDb } from "./tools/config.js";
 import dotenv from "dotenv";
+import * as path from 'path';
 
 const app = express();
 
@@ -17,7 +18,6 @@ treasureRoutes(app);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('front/build'));
 
-  const path = require('path');
   app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'front', 'build', 'index.html'));
   });
