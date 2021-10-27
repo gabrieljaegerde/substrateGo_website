a00338578c3fb23141af93633656423cdf26f35d
gabrieljaegerde <gabrieljaeger1@gmail.com>
2021-10-27 22:10:19 +0200

    mobile rendering fixes

--- front/src/Components/Header.js
+++ front/src/Components/Header.js
@@ -4,7 +4,7 @@ function Header({ isTabletOrMobile, treasureCount }) {
     return (
         <div className="header-bar">
             <img src="logo.jpeg" className="logo" alt="logo" />
-            <span>KusamaGo - NFT Treasure Hunt</span>
+            <span>KusamaGo {isTabletOrMobile ? "" : "- NFT Treasure Hunt"}</span>
             <span className="treasure-count">{treasureCount}
                 <Icon icon="mdi:treasure-chest" color="black" width="22" className="treasure-icon" />
             </span>
